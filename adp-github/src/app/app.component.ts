868d9b408ce958632ee4b225989db2a92f338335
Lincy Varghese <lincytv@gmail.com>
2021-10-27 19:02:29 +0530

    adding button

--- adp-github/src/app/app.component.ts
+++ adp-github/src/app/app.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
 import  { AuthToken } from '@azure/msal-common';
+//import { MSAL_INSTANCE, MsalService } from 'msal';
+import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
+import {FormControl, Validators} from '@angular/forms';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Allianz';
   public username = "";
   public bearer = "";
-  public clinetid = "9d2cd5cd-bb7f-43f8-8e5b-affb0958a2ad";
-  public directoryId = "6e06e42d-6925-47c6-b9e7-9581c7ca302a";
+  public clinetid = "";
+  public directoryId = "";
+  public commandx = "git config --global http.extraHeader \"MFA: bearer ${{bearer}}\“"
+  public  email = new FormControl('', [Validators.required, Validators.email]);
+  public comd = "";
   getusername(){
+    return
+  }
+  getcode(){
 
   }
-
-
+  // getErrorMessage() {
+  //   if (this.email.hasError('required')) {
+  //     return 'You must enter a value';
+  //   }
 }
+export class CardOverviewExample {}
--- adp-github/src/app/app.module.ts
+++ adp-github/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ClipboardModule } from 'ngx-clipboard';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatMenuModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
